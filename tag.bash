5902ae3d885aaa854b0e0e0ae4ba0f8b8d205f8d
yusuke <1319390+yubele@users.noreply.github.com>
2024-08-15 19:59:38 +0900

    Update tag.bash

--- tag.bash
+++ tag.bash
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-from=$(cat Dockerfile | grep FROM | sed -s 's/FROM debian://')
-node=$(cat Dockerfile | grep 'ENV NODE_VERSION' | sed -s 's/ENV NODE_VERSION=//' | sed -s 's/"//g' )
+from=$(cat Dockerfile | grep FROM | sed -s 's/FROM //')
 npm=$(cat Dockerfile | grep 'ENV NPM_VERSION' | sed -s 's/ENV NPM_VERSION=//' | sed -s 's/"//g' )
 ruby=$(cat Dockerfile | grep 'ENV RUBY_VERSION' | sed -s 's/ENV RUBY_VERSION=//' | sed -s 's/"//g' )
 bundler=$(cat Dockerfile | grep 'ENV BUNDLER_VERSION' | sed -s 's/ENV BUNDLER_VERSION=//' | sed -s 's/"//g' )
 
-echo $from-node$node-npm$npm-ruby$ruby-bundler$bundler
+echo $from-node$node-ruby$ruby-bundler$bundler
