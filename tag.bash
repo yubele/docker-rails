442b2fe0f76bcbc4e85517733d29a918d8fde6e2
Yusuke Abe <yusuke@slidict.io>
2025-01-02 13:42:19 +0900

    fix: tags.sh

--- tag.bash
+++ tag.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-from=$(cat Dockerfile | grep FROM | sed -s 's/FROM //')
+from=$(cat Dockerfile | grep FROM | sed -s 's/FROM //' | sed -s 's/:/-/')
 npm=$(cat Dockerfile | grep 'ENV NPM_VERSION' | sed -s 's/ENV NPM_VERSION=//' | sed -s 's/"//g' )
 ruby=$(cat Dockerfile | grep 'ENV RUBY_VERSION' | sed -s 's/ENV RUBY_VERSION=//' | sed -s 's/"//g' )
 bundler=$(cat Dockerfile | grep 'ENV BUNDLER_VERSION' | sed -s 's/ENV BUNDLER_VERSION=//' | sed -s 's/"//g' )
